ce56b456df0d3c5463ae4cd22db00d411c0c5354
Dustin Ingram <dustin.ingram@gmail.com>
2015-03-22 12:19:04 -0400

    Formatting: Flake8 compliance

--- tests/bulbs_tests.py
+++ tests/bulbs_tests.py
@@ -12,24 +12,19 @@ from bulbs.titan.tests.client_tests import titan_client_suite
 
 def suite():
     # This requires Neo4j Server and Rexster are running.
-    
+
     suite = unittest.TestSuite()
 
     suite.addTest(rexster_client_suite())
     suite.addTest(rexster_bulbs_suite())
 
-    suite.addTest(neo4j_client_suite()) 
+    suite.addTest(neo4j_client_suite())
     suite.addTest(neo4j_bulbs_suite())
 
-    suite.addTest(titan_client_suite()) 
+    suite.addTest(titan_client_suite())
     suite.addTest(titan_bulbs_suite())
- 
+
     return suite
 
 if __name__ == '__main__':
     unittest.main(defaultTest='suite')
-
-    
-
-
-
